e135cf450a09a9e5984e78e03cac0f01c13cfc14
suchita.infodales <suchita.infodales@gmail.com>
2024-03-02 18:02:29 +0530

    Updated Website

---
+++ form-handler.php
@@ -0,0 +1,23 @@
+<?php
+$name= $_POST['name'];
+$subject= $_POST['subject'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+
+$email_from="info@infodales.com";
+
+$email_subject="New Form Submission";
+
+$email_body = "User Name : $name .\n User Email : $email .\n Subject : $subject .\n User Message : $message .\n";
+
+$to ="info@infodales.com";
+
+$header = "From : $email_from \r\n";
+
+$headers = "Reply-To : $email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: contact.html")
+
+?>
