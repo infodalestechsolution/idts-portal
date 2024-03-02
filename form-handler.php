d22f1afd936ec52a7107d101c5c66f5fea47b5e7
suchita.infodales <suchita.infodales@gmail.com>
2024-03-02 19:42:40 +0530

    Updated Website

--- form-handler.php
+++ form-handler.php
@@ -1,23 +1,20 @@
 <?php
 $name= $_POST['name'];
 $subject= $_POST['subject'];
-$visitor_email= $_POST['email'];
+$email= $_POST['email'];
 $message= $_POST['message'];
 
 $email_from="info@infodales.com";
 
 $email_subject="New Form Submission";
 
-$email_body = 	"User Name : $name .\n"
-				"User Email : $visitor_email .\n"
-				"Subject : $subject .\n"
-				"User Message : $message .\n";
+$email_body = "User Name : $name .\n User Email : $email .\n Subject : $subject .\n User Message : $message .\n";
 
 $to ="info@infodales.com";
 
 $header = "From : $email_from \r\n";
 
-$headers = "Reply-To : $visitor_email \r\n";
+$headers = "Reply-To : $email \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
 
